62f85fc1679f4b0a14dc3ddef96e7140fdf757fd
Nicolás Ippolito <nico31737@gmail.com>
2023-04-10 11:49:02 -0300

    Fix properati provider

--- providers/properati.py
+++ providers/properati.py
@@ -6,12 +6,8 @@ class Properati(BaseProvider):
     def props_in_source(self, source):
         page_link = self.provider_data['base_url'] + source
         page = 1
-        total_pages = 1
 
         while True:
-            if page > total_pages:
-                break
-
             logging.info("Requesting %s" % page_link)
             page_response = self.request(page_link)
 
@@ -19,23 +15,18 @@ class Properati(BaseProvider):
                 break
 
             page_content = BeautifulSoup(page_response.content, 'lxml')
-            properties = page_content.find_all('div', class_='item-description')
-
-            if page == 1:
-                nav_list = page_content.select('#page-wrapper > div.results-content > div.container.wide-listing > div.content > div.row.items-container > div.item-list.span6 > div > div.pagination.pagination-centered > ul > li')
-                total_pages = len(nav_list) - 2
+            properties = page_content.find_all('div', class_='listing-card')
 
             if len(properties) == 0:
                 break
 
             for prop in properties:
-                link = prop.find('a', class_='item-url')
-                title = link['title']
-                price_section = prop.find('p', class_='price')
-                if price_section is not None:
-                    title = title + ' ' + price_section.get_text().strip()
-                href = link['href']
-                internal_id = prop.find('a', class_='icon-fav')['data-property_id']
+                title = prop.find('div', class_='listing-card__title').get_text().strip()
+                price = prop.find('div', class_='price').get_text().strip()
+                if price is not None:
+                    title = title + ' ' + price
+                href = self.provider_data['base_url'] + prop['data-href']
+                internal_id = prop['data-href'].split('/')[2]
 
                 yield {
                     'title': title,
